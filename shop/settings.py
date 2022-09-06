3854c980ef2714b849a7448cb6077964a0bde5c4
Mohammad Bilal Niazi <44763836+mohammadbilalniazi@users.noreply.github.com>
2022-09-06 17:07:11 +0430

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -95,8 +95,8 @@ DATABASES = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'mohammadbilalniazy@gmail.com'
-EMAIL_HOST_PASSWORD = '0779099940'
+EMAIL_HOST_USER = 'mohammadbilalniazi2016@gmail.com'
+EMAIL_HOST_PASSWORD = 'god_is_one'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
