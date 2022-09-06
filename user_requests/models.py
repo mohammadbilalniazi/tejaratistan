7d7e7451dc336a81e78162909d6794e4dea7d09d
Mohammad Bilal Niazi <44763836+mohammadbilalniazi@users.noreply.github.com>
2022-09-06 22:22:31 +0430

    Update models.py

--- user_requests/models.py
+++ user_requests/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Request(models.Model):
     requester_name=models.CharField(max_length=30)
-    requester_email=models.CharField(max_length=50)
+    requester_email=models.CharField(max_length=50,null=True,blank=True)
+    requester_contact=models.CharField(max_length=20,null=True,blank=True)
     #request_date=models.CharField(max_length=30)
     def __str__(self):
         return self.requester_name
