dde90b43187cdebd9fb1cefdc430568d3233312c
Mohammad Bilal Niazi <44763836+mohammadbilalniazi@users.noreply.github.com>
2022-09-06 16:43:10 +0430

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -92,12 +92,14 @@ DATABASES = {
     }
 } 
 
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_USE_TLS=True
-EMAIL_PORT=587
-EMAIL_HOST_USER='mohammadbilalniazy@gmail.com'
-EMAIL_HOST_PASSWORD='0779099940'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'mohammadbilalniazy@gmail.com'
+EMAIL_HOST_PASSWORD = '0779099940'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
